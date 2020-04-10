0daeaab00e538aa906b06d356e03c59c71a3d8e4
Kumar <adarshkumarsingh83@gmail.com>
2020-04-10 14:33:54 -0500

    adding zipkin

---
+++ spring-cloud-ms/zipkin-server/src/test/java/com/espark/adarsh/ZipkinServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.espark.adarsh;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
