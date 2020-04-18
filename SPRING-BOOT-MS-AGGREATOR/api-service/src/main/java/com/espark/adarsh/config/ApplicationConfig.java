c34dc6245637303a1961ff21c672dbb51a85e441
Kumar <adarshkumarsingh83@gmail.com>
2020-04-18 15:53:07 -0500

    updating doc

--- SPRING-BOOT-MS-AGGREATOR/api-service/src/main/java/com/espark/adarsh/config/ApplicationConfig.java
+++ SPRING-BOOT-MS-AGGREATOR/api-service/src/main/java/com/espark/adarsh/config/ApplicationConfig.java
@@ -1,12 +1,14 @@
 package com.espark.adarsh.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableCircuitBreaker
 public class ApplicationConfig {
 
     @Bean
