0834b617023bb85167df51d048ec6117b96c6617
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2024-05-08 12:13:20 -0500

    workign tested config client for service

--- APPLICATION/2024-ms/espark-api-aggregator/src/main/java/com/espark/adarsh/web/AggregatorController.java
+++ APPLICATION/2024-ms/espark-api-aggregator/src/main/java/com/espark/adarsh/web/AggregatorController.java
@@ -1,13 +1,17 @@
 package com.espark.adarsh.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AggregatorController {
 
+    @Value("${application.message}")
+    String message;
+
     @GetMapping({"/wish", "/greet"})
     public String greet() {
-        return "welcome to espark";
+        return message+" AggregatorController";
     }
 }
